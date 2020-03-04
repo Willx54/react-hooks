730deeeff70d576e988f12565514bd4b0b4f4e05
MrStupid <will.priddle@nelsoncroom.co.uk>
2020-03-04 08:45:21 +0000

    Part way through cursor positioning task

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,13 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+
+  useEffect(() => {
+    document.title = `You have clicked ${count} times`;
+    window.addEventListener("mousemove", handleMouseMove);
+  });
 
   const incrementCount = () => {
-    setCount(count + 1);
+    setCount(prevCount => prevCount + 1);
   };
 
-  return <button onClick={incrementCount}>I was clicked {count} times</button>;
+  const toggleLight = () => {
+    setIsOn(prevIson => !prevIson);
+  };
+
+  return (
+    <>
+      <h2>Counter</h2>
+      <button onClick={incrementCount}>I was clicked {count} times</button>{" "}
+      <h2>Toggle Light</h2>
+      <img
+        src={
+          isOn
+            ? "https://icon.now.sh/highlight/fd0"
+            : "https://icon.now.sh/highlight/aaa"
+        }
+        style={{
+          height: "50px",
+          width: "50px"
+        }}
+        alt="torch"
+        onClick={toggleLight}
+      />
+    </>
+  );
 };
 
 export default App;
