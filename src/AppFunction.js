19d7e15bbe1ee8d43704c2791e5ff44a7562b7ae
Will <w_priddle@hotmail.com>
2020-03-05 09:47:36 +0000

    Geolocation added

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,17 +1,60 @@
 import React, { useState, useEffect } from "react";
 
+const initialLocationState = {
+  latitude: null,
+  longitude: null,
+  speed: null
+}
+
 const App = () => {
   const [count, setCount] = useState(0);
   const [isOn, setIsOn] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+  const [status, setStatus] = useState(navigator.onLine);
+  const [{ latitude, longitude, speed }, setLocation] = useState(initialLocationState);
+  let mounted = true;
 
   useEffect(() => {
     document.title = `You have clicked ${count} times`;
     window.addEventListener("mousemove", handleMouseMove);
-  });
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+    navigator.geolocation.getCurrentPosition(handleGeolocation);
+    const watchId = navigator.geolocation.watchPosition(handleGeolocation);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+      navigator.geolocation.clearWatch(watchId);
+      mounted = false;
+    }
+  }, [count]);
+
+  const handleGeolocation = event => {
+    console.log('event.coords.latitude: ',event.coords.latitude);
+    if (mounted) {
+      setLocation({
+        latitude: event.coords.latitude,
+        longitude: event.coords.longitude,
+        speed: event.coords.speed
+      });
+    }
+  }
+
+  const handleOnline = () => {
+    setStatus(true);
+  }
+
+  const handleOffline = () => {
+    setStatus(false);
+  }
 
   const handleMouseMove = event => {
-    setMousePosition({});
+    setMousePosition({
+      x: event.pageX,
+      y: event.pageY
+    });
   };
 
   const incrementCount = () => {
@@ -40,6 +83,15 @@ const App = () => {
         alt="torch"
         onClick={toggleLight}
       />
+      <h2> Mouse Position</h2>
+      {JSON.stringify(mousePosition, null, 2)}
+      <br />
+      <h2>Network Status</h2>
+      <p>You are <strong>{status ? "online" : "offline"}</strong></p>
+      <h2>Geolocation</h2>
+      <p>Latitude is {latitude}</p>
+      <p>Longitude is {longitude}</p>
+      <p>Your speed is {speed ? speed: "0"}</p>
     </>
   );
 };
